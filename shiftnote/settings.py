3354e46f9d694c2e8655f015b1a1636b525b9888
Tae Park <myfriendtae@gmail.com>
2021-01-07 22:10:33 +1300

    initial commit

--- shiftnote/settings.py
+++ shiftnote/settings.py
@@ -131,10 +131,10 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 )
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = (
-#   os.path.join(BASE_DIR, 'static'),
-# )
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+  os.path.join(BASE_DIR, 'static'),
+)
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
